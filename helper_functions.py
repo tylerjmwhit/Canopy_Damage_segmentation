7badfa00ff0aecdfd36824df1e5620aecc049ab4
Frost274 <sancheev@sonoma.edu>
2022-11-20 18:37:19 -0800

    Load in and create CNN for NAIP dataset

--- helper_functions.py
+++ helper_functions.py
@@ -16,13 +16,16 @@ def dataset_reader(foldername):
     dirname = os.path.join(os.getcwd(), 'Data', foldername)
     images_path = glob.glob(dirname + "/images/*.tif")
     labels_path = glob.glob(dirname + "/labels/*.tif")
+
     numfiles = len(images_path)
     numlabels = len(labels_path)
     # Checking to make sure that there is the same number of labels and images
     assert numlabels == numfiles
+
     img = []
     label = []
     print("reading in %d images" % numfiles)
+
     for i in range(numfiles):
         # works like a percent bar to make sure function did not hang
         if i % 100 == 0:
@@ -33,8 +36,45 @@ def dataset_reader(foldername):
         #im_norm = im_temp / im_temp.max() # max normalizing the image
         img.append(im_temp)
         label.append(lbl_temp[0,0]) # label is made with the (0,0) pixel of label image
+
     img = np.asarray(img)
     label = np.asarray(label)
+
+    return img, label
+
+# This is a function similar to dataset_reader but ignores invalid labels for NAIP data.
+def dataset_reader_naip(foldername):
+    dirname = os.path.join(os.getcwd(), 'Data', foldername)
+    images_path = glob.glob(dirname + "/images/*.tif")
+    labels_path = glob.glob(dirname + "/labels/*.tif")
+    numfiles = len(images_path)
+    numlabels = len(labels_path)
+    # Checking to make sure that there is the same number of labels and images
+
+    assert numlabels == numfiles
+    img = []
+    label = []
+    print("reading in %d images" % numfiles)
+
+    for i in range(numfiles):
+        # works like a percent bar to make sure function did not hang
+        if i % 100 == 0:
+            print("percent complete: {:.0%}".format((i / numfiles)), end="\r")
+
+        # Read in label first to make sure corresponding image and label are valid.
+        lbl_temp = cv2.imread(labels_path[i], cv2.IMREAD_UNCHANGED)
+        if (lbl_temp[0,0] != 0):
+            label.append(lbl_temp[0,0])
+
+            im_temp = cv2.imread(images_path[i],cv2.IMREAD_UNCHANGED)
+            im_temp = cv2.cvtColor(im_temp, cv2.COLOR_BGRA2RGBA)
+            img.append(im_temp)
+    
+    print(f"read in {len(img)} valid images and labels")
+
+    img = np.asarray(img)
+    label = np.asarray(label)
+
     return img, label
 
 def get_simple_model(input_shape):
